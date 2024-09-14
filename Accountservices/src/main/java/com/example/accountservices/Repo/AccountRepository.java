5d4f1fb2d9468ec3758a43c76dfe2ff3cc36cf9b
PraveenKumar <159308991+unique6246@users.noreply.github.com>
2024-09-14 22:06:43 +0530

    all modified

--- Accountservices/src/main/java/com/example/accountservices/Repo/AccountRepository.java
+++ Accountservices/src/main/java/com/example/accountservices/Repo/AccountRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AccountRepository extends JpaRepository<Account, Long> {
     Account findAccountByCustomerId(long customerId);
-    Account findByAccountNumber(String accountNumber);
     Boolean existsAccountByCustomerId(long customerId);
     Boolean existsAccountByAccountNumber(String accountNumber);
     void deleteAccountByAccountNumber(String accountNumber);
